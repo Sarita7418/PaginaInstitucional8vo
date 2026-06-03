1010ce6f0bc30b2104a1afd647f519b5cab685a4
jhovanni <mg6667418@gmail.com>
2026-06-02 23:16:13 -0400

    Finalv2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  output: 'server', // Tu nueva configuración de servidor
-
+  output: 'static', // Tu nueva configuración de servidor
   vite: {
     // @ts-ignore - Silencia el falso error de tipos entre Astro y Tailwind
     plugins: [tailwindcss()]
